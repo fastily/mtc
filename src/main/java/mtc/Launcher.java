f997172b0c9237f87e54bb28f6386bed94055224
fastily <fastily@users.noreply.github.com>
2021-05-02 03:25:00 -0700

    upgrade ui to build with with jdk16
    Also included:
    * Dropped cli, merged shared into main
    * update jar task to work with lastest version
    * bump gradle version

---
+++ src/main/java/mtc/Launcher.java
@@ -0,0 +1,22 @@
+package mtc;
+
+/**
+ * Dummy entry point because JavaFX doesn't play well with fat jars
+ * 
+ * @author Fastily
+ *
+ */
+public class Launcher
+{
+
+	/**
+	 * Main driver
+	 * 
+	 * @param args CLI args, not used
+	 */
+	public static void main(String[] args)
+	{
+		App.main(args);
+	}
+
+}
